205855ced6daa89c28c05f9a2817e56cb4a467e5
Wolviller111 <niklas.scheibner@web.de>
2018-12-12 21:02:30 +0100

    Geschliffenes Deutschklausur

--- app/src/main/java/nac/werewolf/MainActivity.java
+++ app/src/main/java/nac/werewolf/MainActivity.java
@@ -22,7 +22,7 @@ public class MainActivity extends AppCompatActivity {
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action Prechtiger Unterricht", Snackbar.LENGTH_LONG)
+                Snackbar.make(view, "Replace with your own action Prechtiger Unterricht ein geschliffenes Juwel", Snackbar.LENGTH_LONG)
                         .setAction("Action", null).show();
             }
         });
